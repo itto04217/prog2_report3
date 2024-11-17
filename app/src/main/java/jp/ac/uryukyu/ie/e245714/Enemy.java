3fb0a688d887370e505d381bb1c02efffba340f4
Itto Kawamitsu <e245714@ie.u-ryukyu.ac.jp>
2024-11-17 17:05:13 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e245714/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245714/Enemy.java
@@ -35,8 +35,14 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-        hero.wounded(damage);
+        if (this.dead == false){
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            hero.wounded(damage);
+        }
+        else{
+            hero.wounded(0);
+        }
+
     }
 
     /**
